3daecdafe67b3fe1d69506dfd943211549f71c48
Jeongmin Cho <jojm0829@gmail.com>
2024-02-24 02:03:56 +0900

    ✨ feat: RouterProvider 생성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import Swiper from "./components/Swiper";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import PostListPage from "./pages/PostListPage";
 import GlobalStyle from "./styles/GlobalStyle";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <PostListPage />,
+  },
+]);
 
 function App() {
   return (
     <div>
       <GlobalStyle />
-      <Swiper />
+      <RouterProvider router={router} />
     </div>
   );
 }
---
+++ src/pages/PostListPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Swiper from "../components/Swiper";
+
+function PostListPage(props) {
+  return (
+    <div>
+      <Swiper />
+    </div>
+  );
+}
+
+export default PostListPage;
