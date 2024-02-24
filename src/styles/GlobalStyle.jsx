41de9b2d7ba28bbf82d69925e8e6e93585fb4105
Jeongmin Cho <jojm0829@gmail.com>
2024-02-24 16:24:56 +0900

    💄 style: fonts.css 작성
    fonts.css 작성하여 Pretendard 폰트 추가 및 GlobalStyle에 적용
    colors.js 오타 수정

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -4,6 +4,9 @@ import emotionNormalize from "emotion-normalize";
 
 const resetStyle = css`
   ${emotionNormalize};
+  * {
+    font-family: "Pretendard" !important;
+  }
   input {
     border: none;
   }
@@ -13,6 +16,11 @@ const resetStyle = css`
   div,
   input {
     box-sizing: border-box;
+  }
+  ,
+  button {
+    background: none;
+    border: none;
   }
 `;
 
