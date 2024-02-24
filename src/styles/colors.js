1834f81fd6105081cd56a78b147c3f6d64781376
Jeongmin Cho <jojm0829@gmail.com>
2024-02-24 14:53:06 +0900

    💄 style: colors.js 추가

---
+++ src/styles/colors.js
@@ -0,0 +1,17 @@
+export const colors = {
+  darkBlue: "#006CFF",
+  primary1: "##2776E1",
+  primary3: "##86C5FF",
+  primary4: "#EEF2F8",
+  primary5: "##F7F9FF",
+  primary6: "##F1F6FE",
+  grayscale12: "#FCFCFC",
+  grayscale20: "#F1F1F1",
+  grayscale30: "#E8E9E9",
+  grayscale40: "#D5D6D7",
+  grayscale50: "#B5B7B9",
+  grayscale60: "#8B8E91",
+  grayscale70: "#616569",
+  grayscale80: "#393D42",
+  grayscale90: "#23282E",
+};
